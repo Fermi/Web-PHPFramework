3a2f6f06cfa37eece97fc3a48ce4baf2301a69e6
FxYCarl <fxycarl@gmail.com>
2013-03-01 08:37:51 +0800

    Framework Completed

--- Controller/Config/PageConfig.php
+++ Controller/Config/PageConfig.php
@@ -1,6 +1,10 @@
 <?php
-require_once __FILE__.'../../Config/Config.inc.php'
-//本级路径配置.
+require_once dirname(__FILE__).'/Config.php';
+//Options.
+define("BASE_TEMPLATES_DIR",ROOT_DIR.'/Templates');
+define("BASE_SCRIPTS_DIR",ROOT_DIR.'/Statics/Scripts');
+define("BASE_PLUGINS_DIR",ROOT_DIR.'/Plugins');
+define("BASE_WIDGET_DIR",ROOT_DIR.'/Widgets');
 
 
 
--- Controller/Page/Page.class.php
+++ Controller/Page/Page.class.php
@@ -5,23 +5,27 @@
 * Description: 渲染PHP页面模板的控制器基类文件。
 * Date:     Dec 10,2012
 */
-require_once __FILE__.'../Config/PageConfig.inc.php'
+require_once dirname(dirname(__FILE__)).'/Config/PageConfig.php';
 
-class Page
- {
+class Page {
     //整合后模板页储存位置。
-    private renderedPage;
+    private $renderedPage;
     //输出PHP页面私有方法。
     public function renderPage($templateIdentifier,$params = array()){
-        if (!defined("TEMPLATE_DIR")){
-            echo "Undefined 'TEMPLATE_DIR'";
+        if ((!defined("TEMPLATES_DIR"))&&(!defined("BASE_TEMPLATES_DIR"))){
+            echo "Undefined 'TEMPLATES_DIR'";
             exit;
         } else {
             if (isset($params)){
                 extract($params);
             }
+            if(defined("TEMPLATES_DIR")){
+                $renderDir = TEMPLATES_DIR.$templateIdentifier;
+            }else if (defined("BASE_TEMPLATES_DIR")){
+                $renderDir = BASE_TEMPLATES_DIR.$templateIdentifier;
+            }
 
-            $this->renderedPage = file_get_contents(TEMPLATE_DIR.$templateIdentifier);
+            $this->renderedPage = file_get_contents($renderDir);
             
             echo $this->renderedPage;
             exit;
@@ -29,16 +33,21 @@ class Page
     } 
     //加载PHP页面脚本私有方法。
     public function loadScript($scriptIdentifier,$params = array()){
-        if (!defined("SCRIPTS_DIR")){
+        if ((!defined("SCRIPTS_DIR"))&&(!defined("BASE_SCRIPTS_DIR"))){
             echo "Undefined 'SCRIPTS_DIR'";
             exit;
         } else {
             if (isset($params)){
                 extract($params);
             }
+            if(defined("SCRIPTS_DIR")){
+                $renderDir = SCRIPTS_DIR.$scriptIdentifier;
+            }else if (defined("BASE_SCRIPTS_DIR")){
+                $renderDir = BASE_SCRIPTS_DIR.$scriptIdentifier;
+            }
             
-            $renderFile = file_get_contents(SCRIPTS_DIR.$scriptIdentifier);
-            $excute = "\$renderResult = \"$renderFile\";"
+            $renderFile = file_get_contents($renderDir);
+            $excute = '$renderResult = '.$renderFile.';';
 
             @eval($excute);
 
@@ -49,16 +58,20 @@ class Page
     //本方法区别于loadScript方法用return做返回值的原因是，本方法一般不在页面模板里调用。
     //本方法用于生成特定代码并传递给模板等加载。
     public function formatHelper($pluginIdentifier,$params = array()){
-        if (!defined("PLUGINS_DIR")){
+        if ((!defined("PLUGINS_DIR"))&&(!defined("BASE_PLUGINS_DIR"))){
             echo "Undefined 'PLUGINS_DIR'";
             exit;
         } else {
             if (isset($params)){
                 extract($params);
             }
-            
-            $renderFile = file_get_contents(PLUGINS_DIR.$pluginIdentifier);
-            $excute = "\$renderResult = \"$renderFile\";";
+            if(defined("PLUGINS_DIR")){
+                $renderDir = PLUGINS_DIR.$pluginIdentifier;
+            }else if(defined("BASE_PLUGINS_DIR")){
+                $renderDir = BASE_PLUGINS_DIR.$pluginIdentifier;
+            }
+            $renderFile = file_get_contents($renderDir);
+            $excute = '$renderResult = '.$renderFile.';';
 
             @eval($excute);
             
@@ -66,14 +79,14 @@ class Page
          }
     }
     //继承控制器基类的控制器类需要覆盖的默认调用方法。
-    public function initPage(){
+    //public function initPage(){
        //$this->renderPage();
        //exit;
-    }
+    //}
     //继承控制器基类的控制器类需要覆盖的AJAX调用方法。
-    public function ajaxPage(){
+    //public function ajaxPage(){
         //$this->renderPage();
         //exit;
-    }
+    //}
  } 
 
